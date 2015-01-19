e4ef8c5011899ea0683e2b5631213bea31c0f10a
Rl3x Fish ⚓ <RonanCherrueau@gmail.com>
2015-01-19 18:50:59 +0100

    ...

--- PR.scala
+++ PR.scala
@@ -1,8 +1,56 @@
 package phant
 
+/** Pretected Resouce Manager
+  *
+  * A Protected Resource Manager `PR` optimize the impact of applying
+  * multiple protection to one resource and ensure that your program
+  * is correct. A `PR` is made of a Protection `P` and a Resource `R`.
+  *
+  * There are multiple form of protections over resources:
+  * - Raw (nothing)
+  * - Aes (symmetric encryption)
+  * - HesEq/Order/AddMonoid/MulMonoid (homomorphic encryption)
+  * - Frag (data fragmentation)
+  * - Pull (owner bring back data)
+  */
+
+object Protection {
+  import spire.algebra._
+
+  // General trait for resource protection
+  sealed trait Protection[R] { def data: R }
+
+  // Symmetric encryption
+  case class Aes[R](data: R) extends Protection[R]
+
+  // Homomorphic encryption with Eq operation
+  case class HesEq[R: Eq](data: R) extends Protection[R]
+  implicit def heseq[R: Eq]: Eq[HesEq[R]] = new Eq[HesEq[R]] {
+    override def eqv(x: HesEq[R], y: HesEq[R]) =
+      implicitly[Eq[R]].eqv(x.data, y.data)
+  }
+
+  // Homomorphic encryption with Order operations
+  case class HesOrder[R: Order](data: R) extends Protection[R]
+  implicit def hesorder[R: Order]: Order[HesOrder[R]] =
+    new Order[HesOrder[R]] {
+      override def compare(x: HesOrder[R], y: HesOrder[R]) =
+        implicitly[Order[R]].compare(x.data, y.data)
+  }
+
+  // Data fragmentation: We want to split the database on column. We
+  // define a method span, that splits the database into a
+  // prefix/suffix paire (`Chunk`) according to a predicate. A `Chunk`
+  // takes the idea of HList but where Head is a Seq and tail is type
+  // T.
+
+  // Data pulling
+}
+
 object PhantPRTest extends App {
   import spire.algebra._
   import spire.implicits._
+  import Protection._
 
   // ------------------------------------------------------------ Services
   val db: List[(String, String, String)] = List(
@@ -47,35 +95,53 @@ object PhantPRTest extends App {
   // ----------------------------------------------------------- Scenarios
   // Fragmentation, Two can keep a secret:
   object Scenar1 {
+    // Here column splitting is made in many iteration over the
+    // database.
     val split1: List[(Int, String)] =
-      db.zipWithIndex.map { case ((d,_,_), v) => (v+1, d) }
+      db.zipWithIndex.map { case ((d,_,_), v) => (v, d) }
     val split2: List[(Int, String, String)] =
-      db.zipWithIndex.map { case ((_,n,a), v) => (v+1, n, a) }
+      db.zipWithIndex.map { case ((_,n,a), v) => (v, n, a) }
 
     val frag1: List[(Int, String)] = split1
     val frag2: List[(Int, String, String)] = split2
   }
 
   // Fragmentation, Two can keep a secret + Symmetric encryption:
-  case class Sym[A](data: A)
-
   object Scenar2 {
     val split1: List[(Int, String)] =
-      db.zipWithIndex.map { case ((d,_,_), v) => (v+1, d) }
+      db.zipWithIndex.map { case ((d,_,_), v) => (v, d) }
     val split2: List[(Int, String, String)] =
-      db.zipWithIndex.map { case ((_,n,a), v) => (v+1, n, a) }
+      db.zipWithIndex.map { case ((_,n,a), v) => (v, n, a) }
 
     val frag1: List[(Int, String)] = split1
-    val frag2: List[(Int, Sym[String], String)] =
-      split2 map { case (v,n,a) => (v, Sym(n), a) }
+    val frag2: List[(Int, Aes[String], String)] =
+      split2 map { case (v,n,a) => (v, Aes(n), a) }
+  }
+
+  // Fragmentation, multiple fragments
+  object Scenar3 {
+    // Here column splitting is made in one iteration over the
+    // database.
+    val (split1, split2, split3) =
+      db.zipWithIndex.foldLeft[(List[(Int, String)],
+                                List[(Int, String)],
+                                List[(Int, String)])]((Nil,Nil,Nil)) {
+        case ((ds,ns,as), ((d,n,a), v)) => ((v, d) :: ds,
+                                            (v, n) :: ns,
+                                            (v, a) :: as)
+      }
+
+    val frag1: List[(Int, String)] = split1
+    val frag2: List[(Int, String)] = split2
+    val frag3: List[(Int, String)] = split3
   }
 
   // Fragmentation protection
   object JustFrag {
     val split1: List[(Int, String)] =
-      db.zipWithIndex.map { case ((d,_,_), v) => (v+1, d) }
+      db.zipWithIndex.map { case ((d,_,_), v) => (v, d) }
     val split2: List[(Int, String, String)] =
-      db.zipWithIndex.map { case ((_,n,a), v) => (v+1, n, a) }
+      db.zipWithIndex.map { case ((_,n,a), v) => (v, n, a) }
 
     val frag1: List[(Int, String)] = split1
     val frag2: List[(Int, String, String)] = split2
@@ -91,18 +157,11 @@ object PhantPRTest extends App {
   println(JustFrag.mostVisitedClients)
 
   // Fragmentation protection + Homomorphic Order
-  case class HesOrder[F: Order](data: F)
-  implicit def hesorder[F: Order]: Order[HesOrder[F]] =
-    new Order[HesOrder[F]] {
-      override def compare(x: HesOrder[F], y: HesOrder[F]) =
-        implicitly[Order[F]].compare(x.data, y.data)
-  }
-
   object HomomorphicOrder {
     val split1: List[(Int, String)] =
-      db.zipWithIndex.map { case ((d,_,_), v) => (v+1, d) }
+      db.zipWithIndex.map { case ((d,_,_), v) => (v, d) }
     val split2: List[(Int, String, String)] =
-      db.zipWithIndex.map { case ((_,n,a), v) => (v+1, n, a) }
+      db.zipWithIndex.map { case ((_,n,a), v) => (v, n, a) }
 
     val frag1: List[(Int, String)] = split1
     val frag2: List[(Int, HesOrder[String], String)] =
@@ -121,17 +180,11 @@ object PhantPRTest extends App {
 
 
   // Fragmentation protection + Homomorphic Eq and Order
-  case class HesEq[F: Eq](data: F)
-  implicit def heseq[F: Eq]: Eq[HesEq[F]] = new Eq[HesEq[F]] {
-    override def eqv(x: HesEq[F], y: HesEq[F]) =
-      implicitly[Eq[F]].eqv(x.data, y.data)
-  }
-
   object HomomorphicEqToOrder {
     val split1: List[(Int, String)] =
-      db.zipWithIndex.map { case ((d,_,_), v) => (v+1, d) }
+      db.zipWithIndex.map { case ((d,_,_), v) => (v, d) }
     val split2: List[(Int, String, String)] =
-      db.zipWithIndex.map { case ((_,n,a), v) => (v+1, n, a) }
+      db.zipWithIndex.map { case ((_,n,a), v) => (v, n, a) }
 
     // Start with HesEq
     val frag1: List[(Int, String)] = split1
