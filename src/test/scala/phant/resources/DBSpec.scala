cca181c7571964f6a0ad6be53a3ca6552c621992
Rl3x Fish ⚓ <RonanCherrueau@gmail.com>
2015-02-27 12:02:01 +0100

    Better documentation for test cases

--- src/test/scala/phant/resources/DBSpec.scala
+++ src/test/scala/phant/resources/DBSpec.scala
@@ -1,22 +1,24 @@
 package phant
 package resources
 
+import ops.db._
+
 import shapeless.Nat._
 import org.scalatest._
 
-class DBDpec extends FlatSpec with Matchers {
-  val db: String |: Option[String] |: Int |: EOCol = DB(
-    ("2014-01-01", Some("Bob"),   1),
-    ("2014-01-02", Some("Chuck"), 2),
-    ("2014-01-03", Some("Bob"),   3),
-    ("2014-01-04", Some("Chuck"), 4),
-    ("2014-01-05", Some("Bob"),   5),
-    ("2014-01-06", Some("Bob"),   5),
-    ("2014-01-07", None,          5),
-    ("2014-01-08", Some("Bob"),   6),
-    ("2014-01-08", Some("Daan"),  6),
-    ("2014-01-09", Some("Chuck"), 2),
-    ("2014-01-10", Some("Chuck"), 7))
+class DBSpec extends FlatSpec with Matchers {
+  val db: String |: Option[String] |: Int |: EOCol =
+    DB(("2014-01-01", Some("Bob"),   1),
+       ("2014-01-02", Some("Chuck"), 2),
+       ("2014-01-03", Some("Bob"),   3),
+       ("2014-01-04", Some("Chuck"), 4),
+       ("2014-01-05", Some("Bob"),   5),
+       ("2014-01-06", Some("Bob"),   5),
+       ("2014-01-07", None,          5),
+       ("2014-01-08", Some("Bob"),   6),
+       ("2014-01-08", Some("Daan"),  6),
+       ("2014-01-09", Some("Chuck"), 2),
+       ("2014-01-10", Some("Chuck"), 7))
 
   "A DB" should "take columns left to rigth" in {
     db.take(_0) should be (DB())
@@ -98,7 +100,7 @@ class DBDpec extends FlatSpec with Matchers {
     db.drop(_3) should be (DB())
   }
 
-  "A DB" should "be split correctly" in {
+  "A DB" should "produce two chuncks of columns on split" in {
     db.split(_0) should be ((DB(),
                              DB(("2014-01-01", Some("Bob"),   1),
                                 ("2014-01-02", Some("Chuck"), 2),
@@ -173,7 +175,7 @@ class DBDpec extends FlatSpec with Matchers {
                              DB()))
   }
 
-  "A DB" should "be take vertically from top to bottom" in {
+  "A DB" should "take lines from top to bottom" in {
     db.takeV(0) should be (DB[String, Option[String], Int](Nil:_*))
 
     db.takeV(1) should be (DB(("2014-01-01", Some("Bob"),1)))
@@ -181,7 +183,7 @@ class DBDpec extends FlatSpec with Matchers {
     db.takeV(db.head.length) should be (db)
   }
 
-  "A DB" should "be drop vertically from top to bottom" in {
+  "A DB" should "drop lines from top to bottom" in {
     db.dropV(0) should be (db)
 
     db.dropV(1) should be (DB(("2014-01-02", Some("Chuck"), 2),
@@ -199,7 +201,7 @@ class DBDpec extends FlatSpec with Matchers {
                                           Nil:_*))
   }
 
-  "A DB" should "be split vertically correctly" in {
+  "A DB" should "produce two chuncks of DB on splitV" in {
     db.splitV(0) should be ((DB[String, Option[String], Int](Nil:_*), db))
 
     db.splitV(1) should be ((DB(("2014-01-01", Some("Bob"),1)),
