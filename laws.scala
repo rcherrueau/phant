e6c1647f9fc1be49fc1055dc6a5f54f475de04a3
Rl3x Fish ⚓ <ronancherrueau@gmail.com>
2015-03-15 14:42:11 +0100

    Laws for fragmentation

--- laws.scala
+++ laws.scala
@@ -1,109 +1,4 @@
-import shapeless._
-
-// trait Laws {
-//   type Id = Int
-//   val db: List[(String, Unit)]
-//   val frag1: List[(String, Id)]
-//   val frag2: List[(Unit, Id)]
-//   val gdb: List[List[(String, Unit)]]
-//   val gfrag1: List[List[(String, Id)]]
-//   val gfrag2: List[List[(Unit, Id)]]
-
-//   //---------------------------------------------------------------- Id
-//   def frag[A,B](l: List[(A,B)]): (List[(A,Id)], List[(B, Id)])
-//   def unfrag[A,B](f: (List[(A,Id)], List[(B, Id)])): List[(A,B)]
-
-//   // -- identity
-//   unfrag(frag(db)):                            List[(String, Unit)] // =
-//   identity(db):                                List[(String, Unit)]
-//   // --
-
-//   def Π1[A,B]:((A,B)) => A
-//   def Π2[A,B]:((A,B)) => B
-//   def rmId[A]:((A, Id)) => A
-
-//   //------------------------------------------------------------ Project
-//   def project[T,C](l: List[T])(f: T => C): List[C]
-
-//   // -- project on 1
-//   project(unfrag(frag(db)))(Π1):               List[String] // =
-//   project(frag(db)._1)(rmId):                  List[String]
-//   // --
-
-//   // -- project on 2
-//   project(unfrag(frag(db)))(Π2):               List[Unit] // =
-//   project(frag(db)._2)(rmId):                  List[Unit]
-//   // --
-
-//   //------------------------------------------------------------ Select
-//   def select[T](l: List[T])(f1: T => Boolean): List[T]
-//   def slift[T](f: T => Boolean): ((T, Id)) => Boolean
-//   val p1: String => Boolean
-//   val p2: Unit => Boolean
-//   val p12: ((String, Unit)) => Boolean
-
-//   // -- select p1 @ Π1, p2 @ Π2, p12 @ Π12
-//   {
-//     select(unfrag(frag(db))) {
-//       case (s,u) => p1(s) && p2(u) && p12((s,u))
-//     }:                                         List[(String, Unit)] // =
-
-//     // Two possibilities: 1) Use rmId, 2) Lift p*@Π*
-//     val ff1 = select(frag1) (slift(p1))
-//     val ff2 = select(frag2) (slift(p2))
-//     select(unfrag((ff1, ff2))) (p12):          List[(String, Unit)]
-//   }
-//   // --
-
-//   // -- select p1 @ Π1
-//   {
-//     select(unfrag(frag(db))) {
-//       case (s,u) => p1(s)
-//     }:                                         List[(String, Unit)] // =
-
-//     val ff1 = select(frag1) (slift(p1))
-//     val ff2 = identity(frag2)
-//     unfrag((ff1, ff2)):                        List[(String, Unit)]
-//   }
-//   // --
-
-
-//   //------------------------------------------------------------ GroupBy
-//   def groupby[T,U](l: List[T])(f: T => U): List[List[T]]
-//   def mapUnfrag1[A,B](gl: List[List[(A, Id)]],
-//                       lb: List[(B, Id)]): List[List[(A,B)]] =
-//     gl map { la => unfrag(la, lb) }
-
-//   // -- groupby Π1
-//   {
-//     groupby(unfrag(frag(db)))(Π1):             List[List[(String, Unit)]] // =
-
-//     val gf1 = groupby(frag1)(Π1)
-//     val gf2 = identity(frag2)
-//     mapUnfrag1(gf1, gf2):                      List[List[(String, Unit)]]
-//   }
-//   // --
-
-
-//   //--------------------------------------------------------------- Fold
-//   def fold[T,R,E](l: List[T])(f: T => R, g: R => E): List[E]
-//   def flift[T,R](f: List[T] => R): List[(T, Id)] => (R, Id)
-//   val f1: List[String] => String
-//   val f2: List[Unit] => Unit
-//   val f12: ((String, Unit)) => (String, Int)
-
-
-//   {
-//     fold(gdb) (lab => {
-//                  val (la, lb) = lab.unzip
-//                    (f1(la), f2(lb))
-//                }, f12):                        List[(String, Int)] // =
-
-//     val ff1 = fold(gfrag1) (flift(f1), identity[(String, Id)])
-//     val ff2 = fold(gfrag2) (flift(f2), identity[(Unit, Id)])
-//     fold(unfrag((ff1, ff2))) (identity(_), f12)
-//   }
-// }
+import spire.algebra._, spire.implicits._
 
 object Laws {
   type DB[A] = List[A]
@@ -125,7 +20,7 @@ object Laws {
   }
 
   // ------------------------------- Π utils
-  // C should be a subset of T
+  // C ∈ P(T)
   def Π[T, C](db: DB[T])(p: T => C): DB[C] = db.map(p)
 
   def R[A, B]: ((A, B)) => ((A,B)) = identity
@@ -143,19 +38,35 @@ object Laws {
     case (a, b) => f(a) && g(b) && h((a,b))
   }
 
-// def flift[T, R](f: DB[T] => R): DB[(T, Id)] => (R, Id) = ???
-// def fold[T, R, E](l: DB[T])(f: T => R,g: R => E): DB[E] = ???
-// val frag1: DB[(String, Id)] = ???
-// val frag2: DB[(Unit, Id)] = ???
-// val gdb: DB[DB[(String, Unit)]] = ???
-// val gfrag1: DB[DB[(String, Id)]] = ???
-// val gfrag2: DB[DB[(Unit, Id)]] = ???
-// def groupby[T, U](l: DB[T])(f: T => U): DB[DB[T]] = ???
-// val p1: String => Boolean = ???
-// val p12: ((String, Unit)) => Boolean = ???
-// val p2: Unit => Boolean = ???
-// def select[T](l: DB[T])(f1: T => Boolean): DB[T] = ???
+  // ------------------------------- γ utils
+  // U ∈ P(T)
+  def γ[T, U : Eq](db: DB[T])(p: T => U): List[List[T]] = db match {
+    case Nil => Nil
+    case line :: db =>
+      (line :: db.filter(p(_) === p(line))) ::
+        γ(db.filter(p(_) =!= p(line)))(p)
+  }
 
+  def mapUnfrag1[A, B](dbas: DB[DB[(A, Id)]],
+                       dbb: DB[(B, Id)]): DB[DB[(A,B)]] =
+    dbas map { dba => unfrag((dba, dbb)) }
+
+  def mapUnfrag2[A, B](dba: DB[(A, Id)],
+                       dbbs: DB[DB[(B, Id)]]): DB[DB[(A,B)]] =
+    dbbs map { dbb => unfrag((dba, dbb)) }
+
+  // ------------------------------- φ utils
+  // T =:= List[(A, B)]
+  // R =:= (A,B)
+  def φ[T, R, E](l: List[T])(f: T => R, g: R => E): List[E] =
+    l map (f) map (g)
+  // Keeps the least identifier
+  def φlift[T, R](f: List[T] => R): List[(T, Id)] => (R, Id) = tids => {
+    val r = f(tids.unzip._1)
+    val id = tids.minBy(_._2)._2
+
+    (r, id)
+  }
 }
 
 object Check {
@@ -178,35 +89,96 @@ object Check {
       db => unfrag(frag(db)) == db
     }
 
-  val project =
+  val pΠ =
     forAll (db(strInt)) {
+      // Π on relation R
+      db => Π (unfrag(frag(db))) (R) == db
+    } && forAll (db(strInt)) {
+      // Π on relation R1
       db => Π (unfrag(frag(db))) (R1) == Π (frag(db)._1) (rmId)
     } && forAll (db(strInt)) {
+      // Π on relation R2
       db => Π (unfrag(frag(db))) (R2) == Π (frag(db)._2) (rmId)
     }
 
   val p1R1: String => Boolean = _.startsWith("a")
   val p2R2: Int => Boolean = _ % 2 == 0
   val p12R: ((String, Int)) => Boolean = _ => true
-  val select =
+  val pσ =
     forAll (db(strInt)) {
-      // Conjonctive form is threadable
+      // σ on relation R: predicate is in conjonctive form
       db => σ (unfrag(frag(db)))(∧(p1R1, p2R2, p12R)) == {
         val σfrag1 = σ (frag(db)._1) (σlift(p1R1))
         val σfrag2 = σ (frag(db)._2) (σlift(p2R2))
         σ(unfrag((σfrag1, σfrag2))) (p12R)
       }
     } && forAll(db(strInt)) {
+      // σ on relation R1: predicate is in conjonctive form
       db => σ (unfrag(frag(db))) (∧(p1R1, _ => true, _ => true)) == {
         val σfrag1 = σ (frag(db)._1) (σlift(p1R1))
         val σfrag2 = frag(db)._2
         unfrag((σfrag1, σfrag2))
       }
     } && forAll(db(strInt)) {
+      // σ on relation R2: predicate is in conjonctive form
       db => σ (unfrag(frag(db))) (∧(_ => true, p2R2, _ => true)) == {
         val σfrag1 = frag(db)._1
         val σfrag2 = σ (frag(db)._2) (σlift(p2R2))
         unfrag((σfrag1, σfrag2))
+      }
+    }
+
+  val pγ =
+    /*forAll (db(strInt)) {
+      // γ on relation R seems not fragmentable
+      db => γ (unfrag(frag(db))) (R) == {
+        val γfrag1: DB[DB[(String, Id)]]= γ (frag(db)._1) (R1)
+        val γfrag2: DB[DB[(Int, Id)]] = γ (frag(db)._2) (R2)
+
+      }
+    } &&*/ forAll (db(strInt)) {
+      // γ on relation R1
+      db => γ (unfrag(frag(db))) (R1) == {
+        val γfrag1 = γ (frag(db)._1) (R1)
+        val γfrag2 = frag(db)._2
+        mapUnfrag1(γfrag1, γfrag2)
+      }
+    } && forAll (db(strInt)) {
+      // γ on relation R2
+      db => γ (unfrag(frag(db))) (R2) == {
+        val γfrag1 = frag(db)._1
+        val γfrag2 = γ (frag(db)._2) (R2)
+        mapUnfrag2(γfrag1, γfrag2)
+      }
+    }
+
+  val f1R1: List[String] => String = _.head
+  val f2R2: List[Int] => Int = _.head
+  val f12R: ((String, Int)) => (String, Int) = identity
+  val pφ =
+    forAll (db(strInt)) {
+      // φ on relation R1
+      db => φ (γ (unfrag(frag(db))) (R1)) (dbStrInt => {
+                                             val (strs, ints) = dbStrInt.unzip
+                                             (f1R1(strs), f2R2(ints))
+                                           }, f12R) == {
+        val γfrag1 = γ (frag(db)._1) (R1)
+        val γfrag2 = frag(db)._2
+        val φfrag1 = φ (γfrag1) (φlift(f1R1), identity[(String, Id)])
+        val φfrag2 = φ (γfrag2) (identity, identity[(Int, Id)])
+        φ (unfrag((φfrag1, φfrag2))) (identity(_), f12R)
+      }
+    } && forAll (db(strInt)) {
+      // φ on relation R2
+      db => φ (γ (unfrag(frag(db))) (R2)) (dbStrInt => {
+                                             val (strs, ints) = dbStrInt.unzip
+                                             (f1R1(strs), f2R2(ints))
+                                           }, f12R) == {
+        val γfrag1 = frag(db)._1
+        val γfrag2 = γ (frag(db)._2) (R2)
+        val φfrag1 = φ (γfrag1) (identity, identity[(String, Id)])
+        val φfrag2 = φ (γfrag2) (φlift(f2R2), identity[(Int, Id)])
+        φ (unfrag((φfrag1, φfrag2))) (identity(_), f12R)
       }
     }
 
